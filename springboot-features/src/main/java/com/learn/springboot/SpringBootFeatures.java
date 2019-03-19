6ee81e657257c43b6631f3630a6db656155a3b32
xuww <xuww165@163.com>
2019-03-19 14:57:37 +0800

    add sub-module, to show the features of spring boot. 添加子模块，用户演示spring boot的功能。

---
+++ springboot-features/src/main/java/com/learn/springboot/SpringBootFeatures.java
@@ -0,0 +1,25 @@
+package com.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 文件描述
+ *
+ * @ProjectName: learn-springboot
+ * @Package: com.learn.springboot
+ * @Description: note
+ * @Author: alfie-xuww
+ * @CreateDate: 2019/3/19 14:55
+ * @UpdateUser: XUWW
+ * @UpdateDate: 2019/3/19 14:55
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ * <p>
+ * Copyright © 2019 Hundsun Technologies Inc. All Rights Reserved
+ **/
+public class SpringBootFeatures {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFeatures.class);
+    }
+}
